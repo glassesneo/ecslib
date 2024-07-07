04bcbe06cb5fbf38332de36d44c20c343a559672
glassesneo <glassesneo@protonmail.com>
2024-07-07 19:03:11 +0900

    :sparkles: Add support for getting resources in system

--- tests/resource.nim
+++ tests/resource.nim
@@ -13,12 +13,34 @@ type
     language: Language
     soundVolume: range[0..100]
 
+  SoundManager = ref object
+
+proc play(sound: SoundManager, soundVolue: range[0..100]) =
+  echo "playing sound at volume ", soundVolue
+
 let world = World.new()
 
 world.addResource(
   UserSettings(language: Japanese, soundVolume: 50)
 )
 
-echo world.getResource(UserSettings).soundVolume
+let speaker = world.create().attach(SoundManager())
+
+proc playSound(All: [SoundManager]) {.system.} =
+  let settings = command.getResource(UserSettings)
+  for entity in entities:
+    let sound = entity.get(SoundManager)
+    sound.play(settings.soundVolume)
+
+proc turnDownSoundVolume(All: [SoundManager]) {.system.} =
+  let settings = command.getResource(UserSettings)
+  settings.soundVolume -= 5
+
+world.registerSystem(playSound)
+world.registerSystem(turnDownSoundVolume)
+
+for i in 0..<10:
+  world.runSystems()
 
 world.deleteResource(UserSettings)
+
